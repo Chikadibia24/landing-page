c0e3d647ac3b7cd3b8870f1f2d205a5b42252fa1
ChikaDibia <ogbuchika71@gmail.com>
2024-03-16 00:23:44 +0100

    Bug fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,21 @@ function displayMenu() {
   //Get ul element
   let linksContainer = document.getElementById("links-container");
 
-  //let ul = document.getElementsByTagName("ul");
-
   console.log("Function is working");
-  if (linksContainer.style.display === "none" && window.innerWidth > 600) {
-    linksContainer.style.display = "flex";
-  } else if (
-    linksContainer.style.display === "flex" &&
-    window.innerWidth < 600
-  ) {
-    linksContainer.style.display = "none";
-  } else {
-    linksContainer.style.display = "flex";
+
+  if (window.innerHeight > 460) {
+    if (linksContainer.style.display === "none" && window.innerWidth > 1078) {
+      linksContainer.style.display = "flex";
+    } else if (
+      linksContainer.style.display === "flex" &&
+      window.innerWidth < 1078
+    ) {
+      linksContainer.style.display = "none";
+    } else {
+      linksContainer.style.display = "flex";
+    }
   }
+  
 }
 
 window.addEventListener("resize", displayMenu);
